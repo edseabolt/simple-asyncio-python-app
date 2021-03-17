dd188dd4b41ac4e2a57798bb20a326ea7786ee07
Ed Seabolt <eseabolt@us.ibm.com>
2021-03-17 18:46:56 -0500

    initial commit of app

---
+++ main.py
@@ -0,0 +1,15 @@
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+
+@routes.get('/hello')
+async def hello(request: web.Request) -> web.Response:
+    name = request.query.get('name', 'stranger')
+    message = f'Hello {name}!'
+    return web.Response(body=message)
+
+if __name__ == '__main__':
+    app = web.Application()
+    app.add_routes(routes)
+    web.run_app(app, host='0.0.0.0', port=8088)
